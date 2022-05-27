b3e50dfc13af8f6d36ae9635c1901065b680fe77
Ali Merdan <alimerdan87@gmail.com>
2022-05-27 06:57:54 +0200

    -updated error message -updated deployment file

--- Exceptions/NotSupportedAlgorithmException.cs
+++ Exceptions/NotSupportedAlgorithmException.cs
@@ -8,7 +8,7 @@ namespace CAAS.Exceptions
         /// This Exception occurs when the RSA key size is not supported
         /// </summary>
         /// <param name="msg"></param>
-        public NotSupportedAlgorithmException(string msg = "") : base($"Provided algorithm name is not supported.\n {msg}")
+        public NotSupportedAlgorithmException(string msg = "") : base($"Provided algorithm name is not supported. \"{msg}\"")
         {
 
         }
