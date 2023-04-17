4167fd09702319e5bca93a3c4fd3ff1f914b4e23
Ali MERDAN <alimerdan87@gmail.com>
2023-04-17 02:35:35 +0200

    updated

--- src/CAAS/Exceptions/NotSupportedAlgorithmException.cs
+++ src/CAAS/Exceptions/NotSupportedAlgorithmException.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace CAAS.Exceptions
 {
-    [Serializable]
     public class NotSupportedAlgorithmException : Exception
     {
         public NotSupportedAlgorithmException(string msg = "") : base($"Provided algorithm name is not supported. \"{msg}\"")
         {
 
         }
-
-        protected NotSupportedAlgorithmException(SerializationInfo info, StreamingContext context) : base(info, context) { }
-
     }
 }
